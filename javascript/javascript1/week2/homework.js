ab5d34a00e3559b70626d2b502264b8e7744902f
Itsnaklov <outsider90@gmail.com>
2024-05-02 15:06:08 +0200

    Smart-ease Flight booking fullname function

---
+++ javascript/javascript1/week2/homework.js
@@ -0,0 +1,8 @@
+// Flight booking fullname function
+function getFullname(firstname, surname) {
+return firstname + " " + surname;
+}
+getFullname("Benjamin", "Hughes"); // returns "Benjamin Hughes"
+const fullname1 = getFullname("Benjamin", "Hughes");
+const fullname2 = getFullname("Benjamin", "Lazy",);
+console.log (fullname1, fullname2);
