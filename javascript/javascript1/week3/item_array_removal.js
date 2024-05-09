f65a5faff6d7c5697fbacbee5777d02297c3bfb9
Itsnaklov <outsider90@gmail.com>
2024-05-10 01:34:43 +0200

    exercise1-item-array-removal added

---
+++ javascript/javascript1/week3/item_array_removal.js
@@ -0,0 +1,17 @@
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+];
+const nameToRemove = "Ahmad";
+
+// Write some code here
+const filteredNames = names.filter((name) => name !== nameToRemove);
+// Code done
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
